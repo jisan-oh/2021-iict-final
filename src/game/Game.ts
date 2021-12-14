bb664d9bbbc45df32ae72076fd65f79b881fcc57
오지산 <jisan.oh.dev@gmail.com>
2021-12-14 09:45:12 +0900

    추가 선택지, 엔딩 구현

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -38,7 +38,7 @@ class Game {
     const main = shuffle(scripts.main);
     if (main.length > 0) {
       const lastQuestion = main[main.length - 1];
-      lastQuestion.preEffects = ["startOutro"];
+      lastQuestion.preEffects = ["startOutro", "stopCountScore"];
     }
     this.questions = [...this.questions, ...main];
     this.onCardPush();
